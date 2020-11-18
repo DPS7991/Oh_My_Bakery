87dfe2fad255bff2ee74667cb0fe87271510cef2
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-11-18 13:10:31 -0600

    Finished main page

---
+++ bakery-website/src/components/HeroSection/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowDownward,
+  ArrowDown,
+} from "./HeroElements";
+import Video from "../../videos/video.mp4";
+
+const HeroSection = () => {
+  const [hover, sethover] = useState(false);
+
+  const onHover = () => {
+    sethover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Greatest Cakes Ever</HeroH1>
+        <HeroP>
+          Order cakes that make you go 'wow' and receive a 20% discount towards
+          your next order when you sign up with us.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="cakes" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Order now {hover ? <ArrowDownward /> : <ArrowDown />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
